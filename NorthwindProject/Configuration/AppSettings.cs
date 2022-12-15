dfb81b523716ba0df82a2ee00826702fbee75a9e
Sophia <sophia.carbajal1985@gmail.com>
2022-12-14 22:55:48 -0700

    Added AppSettings and DbConnector

---
+++ NorthwindProject/Configuration/AppSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace NorthwindProject.Configuration
+{
+    public class AppSettings
+    {
+        public class ConnectionStringSettings
+        {
+            public string Default
+            {
+                get; set;
+            }
+        }
+        
+        public ConnectionStringSettings ConnectionStrings
+        {
+            get; set;
+        }
+    }
+}
---
+++ NorthwindProject/Models/DbConnector.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using NorthwindProject.Configuration;
+
+namespace NorthwindProject.Models
+{
+    public class DbConnector
+    {
+        private static AppSettings _settings;
+
+        public static void Initialize(AppSettings appSettings)
+        {
+            _settings = appSettings;
+        }
+        public static SqlConnection GetServiceConnection()
+        {
+            //get connection to database
+            string connectionString = _settings.ConnectionStrings.Default;
+            return GetConnection(connectionString);
+        }
+
+        private static SqlConnection GetConnection(string connStr)
+        {
+            //create new connection
+            SqlConnection sqlConn = new SqlConnection(connStr);
+
+            //make sure we're not returning null
+            if (sqlConn == null)
+                throw new Exception("Error connecting to database.");
+
+            //open newly created connection
+            sqlConn.Open();
+
+            return sqlConn;
+        } //end method GetConnection
+    } //end class DbConnector
+}
