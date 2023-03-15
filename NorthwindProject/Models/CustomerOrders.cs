50b74e7cb448db3c7bed7b98f5b400032637d1e8
Sophia <sophia.carbajal1985@gmail.com>
2023-03-15 13:47:07 -0600

    Added the ViewModel for Customer Orders and Customer Info.

---
+++ NorthwindProject/Models/CustomerOrders.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using NorthwindProject.Models.ViewModels;
+
+namespace NorthwindProject.Models
+{
+    public class CustomerOrders
+    {
+        public IEnumerable<CustomerOrders>? customerOrders;
+        public CustomerInfo? customerInfo { get; set; }
+    }
+}
--- NorthwindProject/Models/ViewModels/Orders.cs
+++ NorthwindProject/Models/ViewModels/Orders.cs
@@ -2,5 +2,18 @@
 {
     public class Orders
     {
+        public int OrderID { get; set; }
+        public string CustomerId { get; set; } = string.Empty;
+        public int EmployeeId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime RequiredDate { get; set; }
+        public DateTime ShippedDate { get; set; }
+        public int ShipVia { get; set; }
+        public float Freight { get; set; }
+        public string ShipName { get; set; } = string.Empty;
+        public string ShipAddress { get; set; } = string.Empty;
+        public string ShipCity { get; set; } = string.Empty;
+        public string ShipPostalcode { get; set; } = string.Empty;
+        public string ShipCountry { get; set; } = string.Empty;
     }
 }
